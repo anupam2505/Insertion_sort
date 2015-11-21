4f0be15645dd8453e650b0e2f7bc24f31551ec8c
Anupam Panwar <anupam.panwar@asu.edu>
2015-11-21 16:22:25 -0700

    README.md Updated

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -4,25 +4,26 @@ def insertionSort(ar):
 
 m = input()
 ar = [int(i) for i in raw_input().strip().split()]
-n = m-1
-a = ar[n]
+
 
 
 def insertionSort(ar):
-        for y in range(m-1,0,-1):
-            if (a < ar[y-1] ):
-                ar[y] = ar[y-1]
+            n = len(ar)-1	
+            a = ar[n]
+            for y in range(m-1,0,-1):
+                if (a < ar[y-1] ):
+                    ar[y] = ar[y-1]
+                    arvalue = ' '.join(str(v) for v in ar)
+                    print arvalue
+                elif (a >= ar[y-1]):
+                    ar[y]= a
+                    arvalue = ' '.join(str(v) for v in ar)
+                    print arvalue
+                    return;
+
+            if (a <= ar[0]):
+                ar[0]= a
                 arvalue = ' '.join(str(v) for v in ar)
                 print arvalue
-            elif (a >= ar[y-1]):
-                ar[y]= a
-                arvalue = ' '.join(str(v) for v in ar)
-                print arvalue
-                return;
-       
-        if (a <= ar[0]):
-            ar[0]= a
-            arvalue = ' '.join(str(v) for v in ar)
-            print arvalue
-       
+
 insertionSort(ar)
